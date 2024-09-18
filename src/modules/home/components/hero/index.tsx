8b874a0d0583d32114e7a4d6f92b6a2010a5c92b
Hubert Kolejko <83242010+GhostekTheGuy@users.noreply.github.com>
2024-09-18 16:54:48 +0200

    kanapa

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -5,20 +5,10 @@ const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Ecommerce Starter Template
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Powered by Medusa and Next.js
-          </Heading>
-        </span>
+        <Heading as="h1" className="text-5xl font-bold">
+          <img src="https://cdn.discordapp.com/attachments/1285889428929253386/1285976654602174536/kanapa.png?ex=66ec3a96&is=66eae916&hm=411d0c7fd3d87ccdd67e9cffc94674e4630dc853afbdb307b93298a9f1f002e2" alt="Kanapa" />
+          Twoja treść nagłówka
+        </Heading>
         <a
           href="https://github.com/medusajs/nextjs-starter-medusa"
           target="_blank"
